ab505c0d31baa70b1aa7adff760ec8c90e238875
Lucas Santos <lhs.santoss@gmail.com>
2017-12-03 18:16:32 -0200

    :tada: Initial commit

---
+++ src/query.ts
@@ -0,0 +1,8 @@
+import got from "got";
+import Options from "./interfaces/IOptions";
+import Query from "./interfaces/IQuery";
+import parser from "./modules/parser";
+
+export default (query: Query, options: Options) => {
+ // TODO
+};
