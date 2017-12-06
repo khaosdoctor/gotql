f5a65f5ddde6aa4ea93e0953dc963fdcb50636f9
Lucas Santos <lhs.santoss@gmail.com>
2017-12-05 23:46:44 -0200

    :art: Implemented first signature funct

--- src/mutation.ts
+++ src/mutation.ts
@@ -1,1 +1,8 @@
-// TODO
+import got from "got";
+import IOptions from "./interfaces/IOptions";
+import IQuery from "./interfaces/IQuery";
+import parser from "./modules/parser";
+
+export default (query: IQuery, options: IOptions) => {
+ // TODO
+};
