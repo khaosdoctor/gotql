71162d9e5e9a0399b94ffed6997bad200b6c8f22
Lucas Santos <lhs.santoss@gmail.com>
2017-12-06 22:53:51 -0200

    :construction: Initial implementation for query and mutation

--- src/mutation.ts
+++ src/mutation.ts
@@ -2,7 +2,29 @@ import got from "got";
 import IOptions from "./interfaces/IOptions";
 import IQuery from "./interfaces/IQuery";
 import parser from "./modules/parser";
+import Logger from "./modules/logger";
+import QueryType from "./interfaces/EType";
 
-export default (query: IQuery, options: IOptions) => {
- // TODO
+function mutation (endPoint: string, query: IQuery, options: IOptions) : Promise<object> {
+  try {
+    const logger: Logger = new Logger(options); // Instantiate logger to log messages
+
+    logger.log(`Parsing mutation: ${query}`);
+    const graphQuery: string = parser.parse(query, QueryType.mutation);
+    logger.log(`Parsed mutation: ${graphQuery}`);
+
+    logger.log('Building options object');
+    const httpOptions: object = {
+      headers: options.headers,
+      body: graphQuery,
+      json: true
+    }
+    logger.log(`Option object: ${httpOptions}`);
+
+    return got.post(endPoint, httpOptions);
+  } catch (error) {
+    throw error;
+  }
 };
+
+export default mutation;
