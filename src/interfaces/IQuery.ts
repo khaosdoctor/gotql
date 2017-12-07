61f8ab0e0247c9f0e218328b0556d38413090a39
Lucas Santos <lhs.santoss@gmail.com>
2017-12-07 21:53:59 -0200

    :sparkles: Add support for operation and query args

--- src/interfaces/IQuery.ts
+++ src/interfaces/IQuery.ts
@@ -1,10 +1,11 @@
 import IArgs from "./IArgs";
 import IVars from "./IVars";
 import IFields from "./IField";
+import IOperation from "./IOperation";
 
 export default interface IQuery {
   readonly name?: string;
-  readonly operation: string;
+  readonly operation: string|IOperation;
   args?: IArgs;
   variables?: IVars;
   fields: IFields;
