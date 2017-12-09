d2b781c911992d66ec1dee169b234eb3fae053ff
Lucas Santos <lhs.santoss@gmail.com>
2017-12-09 18:22:02 -0200

    :hammer: Refactored base query and mutation

--- src/query.js
+++ src/query.js
@@ -1,6 +1,44 @@
 const Logger = require('./modules/logger')
 const runner = require('./modules/runner')
 
+/**
+ * @typedef {Object.<string, string>} headerObj
+ */
+/**
+ * @typedef {object} userOpts User Options
+ * @prop {boolean} debug Sets debug mode on/off
+ * @prop {number} errorStatusCode Default error code to send back to the user on error (defaults to 500)
+ * @prop {headerObj} headers Custom headers to be sent along
+ */
+/**
+ * @typedef {object} fieldObj Field properties
+ * @prop {string} [alias] Field Alias
+ * @prop {Object.<string, any>} [args] Field Args
+ * @prop {Object.<string, [fieldObj]>} [fields] Nested fields
+ */
+/**
+ * @typedef {object} operation An operation object
+ * @prop {string} name Operation name
+ * @prop {{Object.<string, any>}} [args] Operation arguments
+ * @prop {Object.<string, [fieldObj]>} fields Field list
+ */
+/**
+ * @typedef {object} queryType An getQL JSON query type
+ * @prop {string} [name] Query name (it is needed when there are multiple queries)
+ * @prop {operation} operation Operation object
+ * @prop {Object.<string, any>} [args] Query arguments
+ * @prop {Object.<string, any>} [variables] Query variables
+ */
+
+/**
+ * Performs a query on the specified GraphQL endpoint
+ *
+ * @param {string} endPoint GraphQL Endpoint
+ * @param {queryType} query The JSON getQL query object
+ * @param {userOpts} [options] User options
+ * @public
+ * @return {Promise<any>} A response object containing all the data
+ */
 function query (endPoint, query, options) {
   const logger = new Logger(options)
   logger.log('Starting a new query')
