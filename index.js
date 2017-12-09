d251d721d527fdfa527d7bed682ffdd7b3d727f9
Lucas Santos <lhs.santoss@gmail.com>
2017-12-09 20:02:30 -0200

    :heavy_plus_sign: Add babel

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const mutation = require('./mutation')
-const query = require('./query')
+const mutation = require('./dist/mutation')
+const query = require('./dist/query')
 
 module.exports = { query, mutation }
