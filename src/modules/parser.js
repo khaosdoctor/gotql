8737bf205ac8aba60d9a4373d7d2bb26d447b09a
Lucas Santos <lhs.santoss@gmail.com>
2017-12-11 00:53:25 -0200

    ⚡️ Add error handling

--- src/modules/parser.js
+++ src/modules/parser.js
@@ -5,7 +5,7 @@ function getQueryVars (variables) {
   for (let varName in variables) {
     queryVars += `$${varName}: ${variables[varName].type}, `
   }
-  return queryVars.slice(0, -2) + ')' // Split last comma
+  return queryVars.slice(0, -2) + ') ' // Split last comma
 }
 
 function getFields (fieldList) {
@@ -19,21 +19,36 @@ function getFields (fieldList) {
 }
 
 function parseOperation (operation) {
-  let operationArgs = ''
-  if (operation.args) {
-    for (let argName in operation.args) {
-      // Check if arg is a variable
-      let argValue = (operation.args[argName].indexOf('$') === 0) ? operation.args[argName] : `"${operation.args[argName]}"`
-      operationArgs += `${argName}: ${argValue}, ` // There'll be a last comma we'll strip later
-    }
-  }
+  if (!operation.name) throw new Error(`name is required for graphQL operation`)
+  if (!operation.fields) throw new Error(`field list is required for operation ${operation.name}`)
 
-  let alias = operation.alias ? `${operation.alias}:` : ''
-  return `${alias} ${operation.name}(${operationArgs.slice(0, -2)}) { ${getFields(operation.fields).trim()} }`.trim()
+  try {
+    let operationArgs = ''
+    if (operation.args) {
+      for (let argName in operation.args) {
+        // Check if arg is a variable
+        let argValue = (operation.args[argName].indexOf('$') === 0) ? operation.args[argName] : `"${operation.args[argName]}"`
+        operationArgs += `${argName}: ${argValue}, ` // There'll be a last comma we'll strip later
+      }
+      operationArgs = `(${operationArgs.slice(0, -2)})`
+    }
+
+    let alias = operation.alias ? `${operation.alias}:` : ''
+    return `${alias} ${operation.name}${operationArgs} { ${getFields(operation.fields).trim()} }`.trim()
+  } catch (error) {
+    throw error
+  }
 }
 
 function parse (query, type) {
-  return `${type} ${query.name || ''}${getQueryVars(query.variables)} { ${parseOperation(query.operation)} }`.trim()
+  try {
+    if (!query.operation) throw new Error('a query must have at least one operation')
+
+    let queryName = (query.name) ? `${query.name} ` : ''
+    return `${type.trim()} ${queryName}${getQueryVars(query.variables)}{ ${parseOperation(query.operation)} }`.trim()
+  } catch (error) {
+    throw new Error(`Parse error: ${error.message}`)
+  }
 }
 
 module.exports = { parse }
