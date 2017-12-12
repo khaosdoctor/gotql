788609c98400a412bdc92b2d7a1e6420135bdb93
Lucas Santos <lhs.santoss@gmail.com>
2017-12-12 00:42:58 -0200

    🚨 Standard Fix

--- src/modules/logger.js
+++ src/modules/logger.js
@@ -1,5 +1,4 @@
 class Logger {
-
   constructor (debug) {
     this._debug = debug || false
   }
@@ -7,7 +6,6 @@ class Logger {
   log (message) {
     if (this._debug) console.log(message)
   }
-
 }
 
 module.exports = Logger
