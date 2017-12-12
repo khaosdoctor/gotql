2dd513d540ad1acdc0cbba11ed021a0052cd5246
Lucas Santos <lhs.santoss@gmail.com>
2017-12-12 00:37:33 -0200

    🏗 Add error handling for vars
    Throws error on non existing variables

--- src/modules/parser.js
+++ src/modules/parser.js
@@ -13,22 +13,38 @@ function getFields (fieldList) {
   let fieldStr = ''
   for (let field of fieldList) {
     if (typeof field === 'string') fieldStr += `${field} ` // Return plain string field
-    else if (typeof field === 'object') fieldStr += `${Object.keys(field)[0]} { ${getFields(field[Object.keys(field)[0]].fields)}} ` // Get fields recursively if nested
+    /* istanbul ignore next */
+    else if (typeof field === 'object') {
+      fieldStr += `${Object.keys(field)[0]} { ${getFields(field[Object.keys(field)[0]].fields)}} ` // Get fields recursively if nested
+    }
   }
   return fieldStr
 }
 
-function parseOperation (operation) {
+function checkArgVar (query, operationArg) {
+  const isVar = (query.operation.args[operationArg].indexOf('$') === 0)
+  let parsedVar = `"${query.operation.args[operationArg]}"`
+
+  if (isVar) {
+    parsedVar = query.operation.args[operationArg]
+    if (!query.variables || !query.variables[operationArg] || !query.variables[operationArg].type || !query.variables[operationArg].value) {
+      throw new Error(`Variable "${operationArg}" is defined on operation but it has neither a type or a value`)
+    }
+  }
+
+  return parsedVar
+}
+
+function parseOperation (query) {
+  let operation = query.operation
   if (!operation.name) throw new Error(`name is required for graphQL operation`)
-  if (!operation.fields) throw new Error(`field list is required for operation ${operation.name}`)
+  if (!operation.fields) throw new Error(`field list is required for operation "${operation.name}"`)
 
   try {
     let operationArgs = ''
     if (operation.args) {
       for (let argName in operation.args) {
-        // Check if arg is a variable
-        let argValue = (operation.args[argName].indexOf('$') === 0) ? operation.args[argName] : `"${operation.args[argName]}"`
-        operationArgs += `${argName}: ${argValue}, ` // There'll be a last comma we'll strip later
+        operationArgs += `${argName}: ${checkArgVar(query, argName)}, ` // There'll be a last comma we'll strip later
       }
       operationArgs = `(${operationArgs.slice(0, -2)})`
     }
@@ -36,7 +52,7 @@ function parseOperation (operation) {
     let alias = operation.alias ? `${operation.alias}:` : ''
     return `${alias} ${operation.name}${operationArgs} { ${getFields(operation.fields).trim()} }`.trim()
   } catch (error) {
-    throw error
+    throw new Error(`Failed to parse operation ${query.operation.name} => ${error.message}`)
   }
 }
 
@@ -45,7 +61,7 @@ function parse (query, type) {
     if (!query.operation) throw new Error('a query must have at least one operation')
 
     let queryName = (query.name) ? `${query.name} ` : ''
-    return `${type.trim()} ${queryName}${getQueryVars(query.variables)}{ ${parseOperation(query.operation)} }`.trim()
+    return `${type.trim()} ${queryName}${getQueryVars(query.variables)}{ ${parseOperation(query)} }`.trim()
   } catch (error) {
     throw new Error(`Parse error: ${error.message}`)
   }
