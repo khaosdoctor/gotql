99fa887a482b69ec4d5f6040f0fea72e8178f4a8
Lucas Santos <lhs.santoss@gmail.com>
2017-12-18 22:17:45 -0200

    💡 Documenting logger

--- src/modules/logger.js
+++ src/modules/logger.js
@@ -1,8 +1,22 @@
+/**
+ * Logger to debug information
+ * @class Logger
+ * @public
+ *
+ */
 class Logger {
+  /**
+   * Constructs Logger
+   * @param {boolean} debug If true, will log
+   */
   constructor (debug) {
     this._debug = debug || false
   }
 
+  /**
+   * Logs a message if Debug is true
+   * @param {string} message Message to be logged
+   */
   log (message) {
     if (this._debug) console.log(message)
   }
