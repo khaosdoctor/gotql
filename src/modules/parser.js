c11b2da29b5ba479b009f2d335fbe3fd1dc5bc34
Lucas Santos <lhs.santoss@gmail.com>
2018-03-17 13:55:28 -0300

    :bulb: Add jsdoc to parser

--- src/modules/parser.js
+++ src/modules/parser.js
@@ -1,4 +1,22 @@
 /**
+ * @typedef {object} fieldObj Field properties
+ * @prop {Array<string | Object.<string, [fieldObj]>>} [fields] Nested fields
+ */
+/**
+ * @typedef {object} operation An operation object
+ * @prop {string} name Operation name
+ * @prop {Object.<string, any> | Object.<string, {value: string, escape: boolean}>} [args] Operation arguments
+ * @prop {string} [alias] Operation arguments
+ * @prop {Array<string | Object.<string, [fieldObj]>>} fields Field list
+ */
+/**
+ * @typedef {object} queryType An getQL JSON query type
+ * @prop {string} [name] Query name (it is needed when there are multiple queries)
+ * @prop {operation} operation Operation object
+ * @prop {Object.<string, { type: string, value: string }>} [variables] Query variables
+ */
+
+/**
  * Parses query variables into strings
  * @param {Object.<string, { type: string, value: string }>} variables Variable object
  * @return {string} Parsed variables
