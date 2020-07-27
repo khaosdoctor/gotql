9fee9e79602a33450afbf2f7f789b168e1081980
Lucas Santos <lhs.santoss@gmail.com>
2020-07-27 15:38:27 -0300

    ✏️ Fix typo

--- src/helpers/literal.ts
+++ src/helpers/literal.ts
@@ -3,12 +3,12 @@ import { LiteralObject } from '../types/Literal'
 /**
  * Literal is a tagged template for an object { value: string, escape: false }
  * @example
- * // literal`example_pkey` return { value: 'example_pkey', escape: false }
+ * // literal`example_pkey` returns { value: 'example_pkey', escape: false }
  * @example
  * // literal`['example_key01', 'example_key02']` return { value: '['example_key01', 'example_key02']', escape: false }
  * @param {TemplateStringsArray} literalValue Literal value
  * @return {LiteralObject} LiteralObject
  */
-export function literal(literalValue: TemplateStringsArray): LiteralObject {
+export function literal (literalValue: TemplateStringsArray): LiteralObject {
   return { value: literalValue && literalValue[0] || '', escape: false }
 }
