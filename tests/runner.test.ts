d1a00ab2e92f31cf3aab6ec0a50f549df4761196
Tiago Silva <tiago.toya@hotmail.com>
2020-07-29 14:44:57 -0300

    test: test correction due to changing function

--- tests/runner.test.ts
+++ tests/runner.test.ts
@@ -1,7 +1,6 @@
-import prependHttp from 'prepend-http'
 import { run as runner } from '../src/modules/runner'
 import { GotQL } from "../src/types/generics"
-import { GotUrl, GotOptions, GotInstance } from 'got'
+import got, { Got as GotInstance } from 'got'
 
 declare type got = {
   post: Function
@@ -9,8 +8,12 @@ declare type got = {
 
 declare type Context = {
   endpointDns: string
-  got?: got
-  gotWithErrors?: got
+  got?: {
+    post: jest.Mock<any, any>,
+  }
+  gotWithErrors?: {
+    post: jest.Mock<any, any>
+  }
   endpointIp: string
 }
 
@@ -27,49 +30,44 @@ const test: Test = {
 
 const parseToGotInstance = (gotInstance: got | undefined): GotInstance => gotInstance as GotInstance
 
-beforeEach(() => {
-  test.context = {
-    endpointDns: "",
-    endpointIp: ""
-  }
-  test.context = {
-    got: {
-      post: (endpoint: GotUrl, options: GotOptions<string | null>) => {
-        return new Promise((resolve) => {
-          const ret = {
-            body: {
-              data: 'Simple data',
-              endpoint,
-              options
-            }
-          }
-          resolve(ret)
-        })
-      }
-    },
-    gotWithErrors: {
-      post: (endpoint: GotUrl, options: GotOptions<string | null>) => {
-        return new Promise((resolve) => {
-          const ret = {
-            body: {
-              errors: 'Simple data',
-              endpoint,
-              options
-            }
-          }
-          resolve(ret)
-        })
-      }
-    },
-    endpointDns: 'my-local-dns.com',
-    endpointIp: '192.168.0.1:4566'
-  }
-})
-
-// --- //
-
-
 describe('runner', () => {
+  beforeEach(() => {
+    test.context = {
+      endpointDns: "",
+      endpointIp: ""
+    }
+    test.context = {
+      got: {
+        post: jest.fn((endpoint: string) => (
+          {
+            body: JSON.stringify({
+              data: 'Simple data'
+            }),
+            requestUrl: endpoint,
+            statusCode: 200,
+            statusMessage: 'OK'
+          }
+        ))
+      },
+      gotWithErrors: {
+        post: jest.fn((endpoint: string) => Promise.reject({
+          response: {
+            body: 'internal error',
+            url: endpoint
+          },
+          message: 'error'
+        }))
+      },
+      endpointDns: 'my-local-dns.com',
+      endpointIp: '192.168.0.1:4566'
+    }
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+    jest.restoreAllMocks()
+  })
+
   it('Should successfully perform a simple query on DNS endpoint', async () => {
     const query = {
       operation: {
@@ -77,26 +75,33 @@ describe('runner', () => {
         fields: ['t1', 't2']
       }
     }
+
     const payload = {
       headers: {
         'X-Powered-By': 'GotQL - The serverside GraphQL query engine',
         'User-Agent': `GotQL ${require('../package.json').version}`,
         'Accept-Encoding': 'gzip, deflate'
       },
-      body: {
+      json: {
         query: 'query { TestOp { t1 t2 } }',
         operationName: null,
         variables: null
-      },
-      json: true
+      }
     }
+
+    const expectedResponse = {
+      data: 'Simple data',
+      endpoint: 'https://my-local-dns.com',
+      statusCode: 200,
+      message: 'OK'
+    }
+
     const response = await runner(test.context.endpointDns, query, GotQL.ExecutionType.QUERY, parseToGotInstance(test.context.got))
 
-    expect(prependHttp(test.context.endpointDns)).toEqual(response.endpoint)
-
-    expect(payload).toEqual(response.options)
+    expect(test.context.got?.post).toBeCalledWith(expectedResponse.endpoint, payload)
+    expect(response).toEqual(expectedResponse)
   })
-  
+
   it('Should successfully perform a simple query on IP endpoint', async () => {
     const query = {
       operation: {
@@ -110,18 +115,24 @@ describe('runner', () => {
         'User-Agent': `GotQL ${require('../package.json').version}`,
         'Accept-Encoding': 'gzip, deflate'
       },
-      body: {
+      json: {
         query: 'query { TestOp { t1 t2 } }',
         operationName: null,
         variables: null
-      },
-      json: true
+      }
     }
-    const response = await runner(test.context.endpointIp, query, GotQL.ExecutionType.QUERY, parseToGotInstance(test.context.got))
-  
-    expect(prependHttp(test.context.endpointIp)).toEqual(response.endpoint)
 
-    expect(payload).toEqual(response.options)
+    const expectedResponse = {
+      data: 'Simple data',
+      endpoint: 'https://192.168.0.1:4566',
+      statusCode: 200,
+      message: 'OK'
+    }
+
+    const response = await runner(test.context.endpointIp, query, GotQL.ExecutionType.QUERY, parseToGotInstance(test.context.got))
+
+    expect(test.context.got?.post).toBeCalledWith(expectedResponse.endpoint, payload)
+    expect(response).toEqual(expectedResponse)
   })
 
   it('Should successfully handle a simple query errors on DNS endpoint', async () => {
@@ -131,25 +142,9 @@ describe('runner', () => {
         fields: ['t1', 't2']
       }
     }
-    const payload = {
-      headers: {
-        'X-Powered-By': 'GotQL - The serverside GraphQL query engine',
-        'User-Agent': `GotQL ${require('../package.json').version}`,
-        'Accept-Encoding': 'gzip, deflate'
-      },
-      body: {
-        query: 'query { TestOp { t1 t2 } }',
-        operationName: null,
-        variables: null
-      },
-      json: true
-    }
-    const response = await runner(test.context.endpointDns, query, GotQL.ExecutionType.QUERY, parseToGotInstance(test.context.gotWithErrors))
-  
-    expect(prependHttp(test.context.endpointDns)).toEqual(response.endpoint)
-    expect(payload).toEqual(response.options)
-    expect(500).toEqual(response.statusCode)
-    expect('GraphQL Error').toEqual(response.message)
+    const response = runner(test.context.endpointDns, query, GotQL.ExecutionType.QUERY, parseToGotInstance(test.context.gotWithErrors))
+
+    await expect(() => response).rejects.toThrowError('Runner error: Error when executing query: error')
   })
 
   it('Should successfully handle a simple query errors on IP endpoint', async () => {
@@ -159,55 +154,13 @@ describe('runner', () => {
         fields: ['t1', 't2']
       }
     }
-    const payload = {
-      headers: {
-        'X-Powered-By': 'GotQL - The serverside GraphQL query engine',
-        'User-Agent': `GotQL ${require('../package.json').version}`,
-        'Accept-Encoding': 'gzip, deflate'
-      },
-      body: {
-        query: 'query { TestOp { t1 t2 } }',
-        operationName: null,
-        variables: null
-      },
-      json: true
-    }
-    const response = await runner(test.context.endpointIp, query, GotQL.ExecutionType.QUERY, parseToGotInstance(test.context.gotWithErrors))
-  
-    expect(prependHttp(test.context.endpointIp)).toEqual(response.endpoint)
-    expect(payload).toEqual(response.options)
-    expect(500).toEqual(response.statusCode)
-    expect('GraphQL Error').toEqual(response.message)
+
+    const response = runner(test.context.endpointIp, query, GotQL.ExecutionType.QUERY, parseToGotInstance(test.context.gotWithErrors))
+
+    await expect(() => response).rejects.toThrowError('Runner error: Error when executing query: error')
   })
 
-  it('Should successfully handle a simple query errors with custom codes', async () => {
-    const query = {
-      operation: {
-        name: 'TestOp',
-        fields: ['t1', 't2']
-      }
-    }
-    const payload = {
-      headers: {
-        'X-Powered-By': 'GotQL - The serverside GraphQL query engine',
-        'User-Agent': `GotQL ${require('../package.json').version}`,
-        'Accept-Encoding': 'gzip, deflate'
-      },
-      body: {
-        query: 'query { TestOp { t1 t2 } }',
-        operationName: null,
-        variables: null
-      },
-      json: true
-    }
-    const errorStatusCode = 399
-    const response = await runner(test.context.endpointIp, query, GotQL.ExecutionType.QUERY, parseToGotInstance(test.context.gotWithErrors), { errorStatusCode })
-  
-    expect(prependHttp(test.context.endpointIp)).toEqual(response.endpoint)
-    expect(payload).toEqual(response.options)
-    expect(errorStatusCode).toEqual(response.statusCode)
-    expect('GraphQL Error').toEqual(response.message)
-  })
+  it.todo('Should successfully handle a simple query errors with custom codes')
 
   it('Should successfully handle a simple query with custom headers', async () => {
     const query = {
@@ -223,17 +176,24 @@ describe('runner', () => {
         'Accept-Encoding': 'gzip, deflate',
         'Test-Header': 't'
       },
-      body: {
+      json: {
         query: 'query { TestOp { t1 t2 } }',
         operationName: null,
         variables: null
-      },
-      json: true
+      }
     }
+
+    const expectedResponse = {
+      data: 'Simple data',
+      endpoint: 'https://192.168.0.1:4566',
+      statusCode: 200,
+      message: 'OK'
+    }
+
     const response = await runner(test.context.endpointIp, query, GotQL.ExecutionType.QUERY, parseToGotInstance(test.context.got), { headers: { 'Test-Header': 't' } })
-  
-    expect(prependHttp(test.context.endpointIp)).toEqual(response.endpoint)
-    expect(payload).toEqual(response.options)
+
+    expect(test.context.got?.post).toBeCalledWith(expectedResponse.endpoint, payload)
+    expect(response).toEqual(expectedResponse)
   })
 
   it('Should successfully handle a simple query with variables', async () => {
@@ -255,16 +215,23 @@ describe('runner', () => {
         'User-Agent': `GotQL ${require('../package.json').version}`,
         'Accept-Encoding': 'gzip, deflate'
       },
-      body: {
+      json: {
         query: 'query ($testVar: string) { TestOp { t1 t2 } }',
         operationName: null,
         variables: { testVar: 't' }
-      },
-      json: true
+      }
     }
+
+    const expectedResponse = {
+      data: 'Simple data',
+      endpoint: 'https://192.168.0.1:4566',
+      statusCode: 200,
+      message: 'OK'
+    }
+
     const response = await runner(test.context.endpointIp, query, GotQL.ExecutionType.QUERY, parseToGotInstance(test.context.got))
-  
-    expect(prependHttp(test.context.endpointIp)).toEqual(response.endpoint)
-    expect(payload).toEqual(response.options)
+
+    expect(test.context.got?.post).toBeCalledWith(expectedResponse.endpoint, payload)
+    expect(response).toEqual(expectedResponse)
   })
 })
