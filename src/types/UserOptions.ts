32e9d22126ad20165766eda7c9591fe39d88fcbf
Tiago Silva <tiago.toya@hotmail.com>
2020-07-29 14:42:53 -0300

    fix: typing adjustment

--- src/types/UserOptions.ts
+++ src/types/UserOptions.ts
@@ -1,4 +1,4 @@
-import got = require('got')
+import { Got as GotInstance } from 'got'
 
 /**
  * @typedef {object} userOpts User Options
@@ -11,5 +11,5 @@ export type UserOptions = {
   headers?: {
     [name: string]: string
   },
-  gotInstance?: got.GotInstance
+  gotInstance?: GotInstance
 }
